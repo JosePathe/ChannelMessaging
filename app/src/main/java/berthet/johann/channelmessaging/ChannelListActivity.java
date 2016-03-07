d58f863c9d50dd15dc8ab172fc4ccad7ab9a0c82
Johann <johann.brth@gmail.com>
2016-03-07 10:22:19 +0100

    first fragment added

--- app/src/main/java/berthet/johann/channelmessaging/ChannelListActivity.java
+++ app/src/main/java/berthet/johann/channelmessaging/ChannelListActivity.java
@@ -3,6 +3,7 @@ package berthet.johann.channelmessaging;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +14,7 @@ import com.google.gson.Gson;
 
 import java.util.HashMap;
 
+import berthet.johann.channelmessaging.fragment.ChannelListFragment;
 import berthet.johann.channelmessaging.network.ChannelList;
 import berthet.johann.channelmessaging.network.WSRequestHandler;
 import berthet.johann.channelmessaging.network.onWSRequestListener;
@@ -20,17 +22,17 @@ import berthet.johann.channelmessaging.network.onWSRequestListener;
 /**
  * Created by Johann on 08/02/2016.
  */
-public class ChannelListActivity extends AppCompatActivity implements onWSRequestListener, AdapterView.OnItemClickListener {
+public class ChannelListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private static final String PREFS_ACCESS_TOKEN = "MyAccessToken";
-    private ListView myListView;
+    //private ListView myListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_channel);
-        myListView = (ListView)findViewById(R.id.channelListView);
-        myListView.setOnItemClickListener(this);
-        
+        //myListView = (ListView)findViewById(R.id.channelListView);
+        //myListView.setOnItemClickListener(this);
+        /*
         SharedPreferences settings = getSharedPreferences(PREFS_ACCESS_TOKEN, 0);
         String myAccessToken = settings.getString("accesstoken", null);
 
@@ -39,27 +41,32 @@ public class ChannelListActivity extends AppCompatActivity implements onWSReques
 
         WSRequestHandler getChannelListRequest = new WSRequestHandler(params, "http://www.raphaelbischof.fr/messaging/?function=getchannels", 0);
         getChannelListRequest.setOnWSRequestListener((onWSRequestListener) this);
-        getChannelListRequest.execute();
+        getChannelListRequest.execute();*/
     }
-
+    /*
     @Override
     public void onWSRequestCompleted(int requestCode, String response) {
         Gson gson = new Gson();
         ChannelList myList = new ChannelList();
         myList.channels = gson.fromJson(response, ChannelList.class).channels;
-        myListView.setAdapter(new ChannelAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, myList.channels));
+        //myListView.setAdapter(new ChannelAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, myList.channels));
     }
 
     @Override
     public void onWSRequestError(int requestCode, String exception) {
         Toast.makeText(getApplicationContext(), exception, Toast.LENGTH_SHORT).show();
-    }
+    }*/
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        long channelID = myListView.getAdapter().getItemId(position);
-        Intent myIntent = new Intent(ChannelListActivity.this, ChannelActivity.class);
-        myIntent.putExtra("channelID", channelID); //Optional parameters
-        startActivity(myIntent);
+        //long channelID = myListView.getAdapter().getItemId(position);
+        Fragment listChannel = (ChannelListFragment) getSupportFragmentManager().findFragmentById(R.id.channelListFragment);
+        if (listChannel.isInLayout()) {
+            long channelID = id;
+            Intent myIntent = new Intent(ChannelListActivity.this, ChannelActivity.class);
+            myIntent.putExtra("channelID", channelID); //Optional parameters
+            startActivity(myIntent);
+        }
+
     }
 }
---
+++ app/src/main/java/berthet/johann/channelmessaging/fragment/ChannelListFragment.java
@@ -0,0 +1,63 @@
+package berthet.johann.channelmessaging.fragment;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+import berthet.johann.channelmessaging.ChannelAdapter;
+import berthet.johann.channelmessaging.ChannelListActivity;
+import berthet.johann.channelmessaging.R;
+import berthet.johann.channelmessaging.network.ChannelList;
+import berthet.johann.channelmessaging.network.WSRequestHandler;
+import berthet.johann.channelmessaging.network.onWSRequestListener;
+
+/**
+ * Created by Johann on 01/03/2016.
+ */
+public class ChannelListFragment extends Fragment implements onWSRequestListener {
+    private static final String PREFS_ACCESS_TOKEN = "MyAccessToken";
+
+    private ListView lvFragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_listchannel,container);
+        lvFragment = (ListView)v.findViewById(R.id.lvFragment);
+        lvFragment.setOnItemClickListener((ChannelListActivity)getActivity());
+
+        SharedPreferences settings = getContext().getSharedPreferences(PREFS_ACCESS_TOKEN, 0);
+        String myAccessToken = settings.getString("accesstoken", null);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("accesstoken", myAccessToken);
+
+        WSRequestHandler getChannelListRequest = new WSRequestHandler(params, "http://www.raphaelbischof.fr/messaging/?function=getchannels", 0);
+        getChannelListRequest.setOnWSRequestListener((onWSRequestListener) this);
+        getChannelListRequest.execute();
+
+        return v;
+    }
+
+    @Override
+    public void onWSRequestCompleted(int requestCode, String response) {
+        Gson gson = new Gson();
+        ChannelList myList = new ChannelList();
+        myList.channels = gson.fromJson(response, ChannelList.class).channels;
+        lvFragment.setAdapter(new ChannelAdapter(getContext(), android.R.layout.simple_list_item_1, myList.channels));
+    }
+
+    @Override
+    public void onWSRequestError(int requestCode, String exception) {
+        Toast.makeText(getContext(), exception, Toast.LENGTH_SHORT).show();
+    }
+
+}
